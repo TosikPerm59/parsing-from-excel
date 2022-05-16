b47bb6fdc293475c8928f6ebdac41a3badb2c9a3
Tosikperm59 <tosikperm59@gmail.com>
2022-05-16 16:27:09 +0500

    Make new project strucrure

--- draft.py
+++ draft.py
@@ -1,7 +1,6 @@
 import openpyxl
 import warnings
 import os
-
 warnings.simplefilter("ignore")
 
 adress_file_from_giis = ('E:\Elena\Downloads\/' +
@@ -12,11 +11,37 @@ file_giis = openpyxl.open(adress_file_from_giis)
 sheet = file_giis.active
 sheet.delete_rows(1, 3)
 giis_dict = {}
+
+def check_number(st):
+    true_lst = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    for i in st:
+        if i not in true_lst:
+            return False
+    else:
+        return True
+
+
+def check_description(self, description):
+    for elem in description.split(' '):
+        if len(elem) == 13:
+            if check_number(elem):
+                self.value[id] = elem
+
+
+
+class Jewelry:
+    def init(self, name, description, weight, metall):
+        pass
+
+
+
+
 for row in range(1, sheet.max_row + 1):
     giis_dict[sheet[row][0].value] = ({'UIN': sheet[row][1].value, 'Наименование': sheet[row][2].value,
                                        'Описание': sheet[row][3].value, 'Масса': sheet[row][4].value,
                                        'Металл': sheet[row][5].value})
 
-print(giis_dict)
+for elem in giis_dict.values():
+    print(elem)
 
-input('Press ENTER to exit')
+
