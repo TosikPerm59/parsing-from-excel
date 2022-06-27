1e6f0deb62fd153e08aa551e2016c5d2086faf64
Tosikperm59 <tosikperm59@gmail.com>
2022-06-28 00:44:04 +0500

    Допилил парсинг xlsx.

--- servise_files/invoiceParser.py
+++ servise_files/invoiceParser.py
@@ -5,7 +5,6 @@ from inputs import input_giis_file_path
 
 
 def invoice_parsing(path_to_excel_file):
-
     rows_list, sheet, file_type, file_name, file_path = excelReader.read_excel_file(path_to_excel_file)
     product_ind = weight_ind = price_ind = prod_uin = start = finish = price_per_gram_ind = None
     new_excel_file = Workbook()
@@ -95,6 +94,7 @@ def invoice_parsing(path_to_excel_file):
 
     if file_type == '.xlsx':
         cols = sheet.max_column
+        product_ind = weight_ind = price_ind = prod_uin = start = finish = price_per_gram_ind = None
         rows_lst = []
         for row in rows_list:
             row_lst = []
@@ -113,17 +113,9 @@ def invoice_parsing(path_to_excel_file):
 
         header = rows_lst[start: start + 3]
         rows_lst = rows_lst[start + 3: finish]
-
-        counter = 1
-        list_r = []
-        for row in rows_lst:
-            if row[0] == counter:
-                list_r.append(row)
-                counter += 1
+        giis_list = giisParser.giis_file_parsing(input_giis_file_path())
 
         for row in header:
-            print(row)
-
             for elem in row:
                 if elem is not None:
                     origin_elem = elem
@@ -141,10 +133,59 @@ def invoice_parsing(path_to_excel_file):
                     if r_elem.find('цена') != -1 or elem.find('цена') != -1:
                         price_per_gram_ind = row.index(origin_elem)
 
-        print(product_ind, weight_ind, price_ind, price_per_gram_ind)
+        counter = 0
+        list_r = []
+        for product in rows_lst:
+            if product[0] == counter + 1:
+                counter += 1
+                description_string = product[product_ind].lower()
+
+                prod_name = find_name(description_string)
+                prod_metal = find_metal(description_string)
+                prod_size = find_size(description_string, group='excel')
+                prod_weight = product[weight_ind]
+                prod_art = find_art(description_string, group='excel')
+                prod_barcode = find_barcode(description_string)
+                prod_price = product[price_ind]
+
+                if not prod_metal:
+                    if product[price_per_gram_ind] > 2500:
+                        prod_metal = 'Золото 585'
+                    else:
+                        prod_metal = 'Серебро 925'
+
+                product_description = f'{prod_name}, {prod_metal} ('
+                if prod_art:
+                    product_description = f'{product_description}арт. {prod_art}'
+                if prod_barcode:
+                    prod_uin, string_number = find_uin(prod_barcode, giis_list)
+                    if prod_uin:
+                        product_description = f'{product_description}, уин {prod_uin}'
+                product_description = f'{product_description}, вес {prod_weight} г.'
+
+                if prod_name.lower() in products_with_size.keys():
+                    if not prod_size:
+                        print(f'\nПрограмме не удалось определить размер для {products_with_size[prod_name.lower()]}.')
+                        print(f'Укажите размер для изделия: {product_description}')
+                        prod_size = input('Введите размер в формате 00.0: ')
+                if prod_size:
+                    product_description = f'{product_description}, р-р {prod_size}'
+                product_description = f'{product_description})'
+                print(product_description)
+
+                new_excel_sheet['A' + str(counter)] = product_description
+                new_excel_sheet['B' + str(counter)] = prod_barcode
+                new_excel_sheet['C' + str(counter)] = prod_uin
+                new_excel_sheet['D' + str(counter)] = 'Товар'
+                new_excel_sheet['E' + str(counter)] = prod_price
+                new_excel_sheet['F' + str(counter)] = prod_metal.split(' ')[0]
+                new_excel_sheet['G' + str(counter)] = prod_name
+
+                prod_name = prod_metal = prod_inserts = prod_weaving = prod_art = prod_uin = prod_barcode = None
 
     path_for_save = f'{file_path}\\Номенклатура файла ({file_name}).xlsx'
-    # new_excel_file.save(path_for_save)
+    new_excel_file.save(path_for_save)
 
 
-# invoice_parsing('E:\Elena\Documents\Документы  Александрова Е.П\Накладные\Входящие накладные\Белышева Юлия\Аена 99.xlsx')
+# invoice_parsing(
+#     'E:\Elena\Documents\Документы  Александрова Е.П\Накладные\Входящие накладные\Белышева Юлия\Аена 99.xlsx')
